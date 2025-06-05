24a96b85ae134744053b72701edff42f5077497e
Christian Flessa <chris@z0rn.net>
2025-06-05 23:44:02 +0200

    Fix setters for BlazorMessageBusOptions

--- src/BlazorMessageBus/BlazorMessageBusOptions.cs
+++ src/BlazorMessageBus/BlazorMessageBusOptions.cs
@@ -11,10 +11,10 @@ public record BlazorMessageBusOptions
     /// If <see langword="true"/>, publishing will stop on the first handler exception. If <see langword="true"/>, all handlers are invoked.
     /// Default: <see langword="false"/>.
     /// </summary>
-    public Boolean StopOnFirstError { get; init; }
+    public Boolean StopOnFirstError { get; set; }
 
     /// <summary>
     /// An optional default exception handler for publish errors.
     /// </summary>
-    public Func<Exception, Task>? OnPublishException { get; init; }
+    public Func<Exception, Task>? OnPublishException { get; set; }
 }
