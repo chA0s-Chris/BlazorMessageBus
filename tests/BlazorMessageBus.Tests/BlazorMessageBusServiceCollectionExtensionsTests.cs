c50f35764ba34a6a9154ff8bfee87a3e786d2cd7
Christian Flessa <chris@z0rn.net>
2025-06-05 23:44:35 +0200

    Add unit tests for BlazorMessageBus service collection extensions

---
+++ tests/BlazorMessageBus.Tests/BlazorMessageBusServiceCollectionExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2025 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+namespace Cha0s.BlazorMessageBus;
+
+using Chaos.BlazorMessageBus;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+
+public class BlazorMessageBusServiceCollectionExtensionsTests
+{
+    private IServiceCollection _services;
+
+    [SetUp]
+    public void Setup() => _services = new ServiceCollection();
+
+    [Test]
+    public void AddBlazorMessageBus_ShouldRegisterServices()
+    {
+        _services.AddBlazorMessageBus();
+
+        var serviceProvider = _services.BuildServiceProvider();
+
+        serviceProvider.GetService<IBlazorMessageBus>().Should().NotBeNull();
+        serviceProvider.GetService<IBlazorMessageExchange>().Should().NotBeNull();
+    }
+
+    [Test]
+    public void AddBlazorMessageBus_WithConfigureAction_ShouldInvokeConfigureActionAndRegisterOptions()
+    {
+        _services.AddBlazorMessageBus(options =>
+        {
+            options.StopOnFirstError = true;
+            options.OnPublishException = _ => Task.CompletedTask;
+        });
+
+        var serviceProvider = _services.BuildServiceProvider();
+        var options = serviceProvider.GetService<IOptions<BlazorMessageBusOptions>>();
+        options.Should().NotBeNull();
+        options.Value.StopOnFirstError.Should().BeTrue();
+        options.Value.OnPublishException.Should().NotBeNull();
+    }
+}
