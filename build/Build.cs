2a717631632f56b0a35ca31353788b780ec6646a
Christian Flessa <chris@z0rn.net>
2025-06-08 16:52:40 +0200

    Add coverage reporting and packaging configuration to build pipeline

--- build/Build.cs
+++ build/Build.cs
@@ -7,19 +7,28 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.ReportGenerator;
+using Nuke.Common.Utilities;
+using Nuke.Common.Utilities.Collections;
 using Nuke.Components;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json;
 
 internal class Build : NukeBuild,
                        IHazArtifacts,
                        IHazSolution,
                        IHazGitRepository,
                        ICompile,
-                       ITest
+                       ITest,
+                       IPack,
+                       IPublish,
+                       IReportCoverage
 {
     private Target Clean => target =>
         target.Before<IRestore>()
@@ -35,6 +44,8 @@ internal class Build : NukeBuild,
 
     private AbsolutePath SourceDirectory => RootDirectory / "src";
 
+    private AbsolutePath CoverageSummary => From<IReportCoverage>().CoverageReportDirectory / "Summary.json";
+
     public Configure<DotNetBuildSettings> CompileSettings => settings =>
         settings.EnableContinuousIntegrationBuild()
                 .EnableTreatWarningsAsErrors();
@@ -42,7 +53,62 @@ internal class Build : NukeBuild,
     public Configure<DotNetPublishSettings> PublishSettings => settings =>
         settings.EnableContinuousIntegrationBuild();
 
+
     public Configuration Configuration => Configuration.Release;
+
+    public Configure<DotNetPackSettings> PackSettings => settings =>
+        settings.DisablePackageRequireLicenseAcceptance()
+                .EnableContinuousIntegrationBuild()
+                .ResetNoBuild()
+                .SetProject("src/BlazorMessageBus/BlazorMessageBus.csproj")
+                .AddProperty("AdditionalConstants", "NUGET_RELEASE")
+                .AddProperty("SignAssembly", "true")
+                .AddProperty("AssemblyOriginatorKeyFile", "../../BlazorMessageBus.snk")
+                .EnableIncludeSymbols()
+                .SetSymbolPackageFormat(DotNetSymbolPackageFormat.snupkg);
+
+    public Configure<DotNetNuGetPushSettings> PushSettings => settings =>
+        settings.EnableSkipDuplicate();
+
+    public Configure<ReportGeneratorSettings> ReportGeneratorSettings => settings =>
+        settings.SetReports(From<IReportCoverage>().TestResultDirectory / "**/coverage.cobertura.xml")
+                .SetReportTypes(ReportTypes.JsonSummary);
+
+    public Target ReportCoverage => target =>
+        target.Inherit<IReportCoverage>()
+              .Executes(() =>
+              {
+                  var coverage = "unknown";
+
+                  try
+                  {
+                      var json = File.ReadAllText(CoverageSummary);
+                      using var jsonDocument = JsonDocument.Parse(json);
+
+                      if (jsonDocument.RootElement.TryGetProperty("summary", out var summary) &&
+                          summary.TryGetProperty("linecoverage", out var lineCoverage))
+                      {
+                          coverage = $"{lineCoverage.GetDouble().ToString("#.0", CultureInfo.InvariantCulture)}%";
+                      }
+                  }
+                  catch (Exception e)
+                  {
+                      Log.Error(e, "Failed to read coverage summary.");
+                  }
+
+                  ReportSummary(config => config.AddPair("Coverage", coverage));
+              });
+
+    Boolean IReportCoverage.CreateCoverageHtmlReport => true;
+
+
+    Boolean IReportCoverage.ReportToCodecov => false;
+
+    public Configure<DotNetTestSettings> TestSettings => settings =>
+        settings.EnableNoBuild()
+                .When(InvokedTargets.Contains(((IReportCoverage)this).ReportCoverage), transform =>
+                          transform.SetDataCollector("XPlat Code Coverage")
+                                   .SetSettingsFile("coverlet.xml"));
 
     public IEnumerable<Project> TestProjects => GetTestProjects().ToList();
 
@@ -72,4 +138,6 @@ internal class Build : NukeBuild,
     }
 
     public static Int32 Main() => Execute<Build>(x => ((ICompile)x).Compile);
+
+    private T From<T>() where T : INukeBuild => (T)(Object)this;
 }
