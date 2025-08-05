e9a0662f3ab8fff4ff17aa84e98717dc50209ae3
Christian Flessa <chris@z0rn.net>
2025-08-05 23:17:08 +0200

    Add message bridge and bridgeable message bus implementations

---
+++ src/BlazorMessageBus/Bridging/BridgeMessageHandler.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2025 Christian Flessa. All rights reserved.
+// This file is licensed under the MIT license. See LICENSE in the project root for more information.
+namespace Chaos.BlazorMessageBus.Bridging;
+
+/// <summary>
+/// Delegate for handling outbound messages from the message bridge.
+/// </summary>
+/// <param name="messageType">The type of the message being bridged.</param>
+/// <param name="payload">The message payload.</param>
+/// <param name="cancellationToken">Cancellation token for the operation.</param>
+/// <returns>A task representing the asynchronous bridge operation.</returns>
+public delegate Task BridgeMessageHandler(Type messageType, Object payload, CancellationToken cancellationToken = default);
